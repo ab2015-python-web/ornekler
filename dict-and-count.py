1fa97c74fe028866ac03568e69e9e33da3e351d2
Fatih Erikli <fatiherikli@gmail.com>
2015-02-01 11:44:10 +0200

    Clean dict and count example

---
+++ dict-and-count.py
@@ -0,0 +1,9 @@
+kelimeler = open("text.txt", "r").read().split(" ")
+dizi = {}
+
+for kelime in kelimeler:
+    if (len(kelime) > 3):
+        dizi[kelime] = kelimeler.count(kelime)
+
+for (key, val) in dizi.items():
+    print "%15s kelime : %s" %(key, "*" * val)
