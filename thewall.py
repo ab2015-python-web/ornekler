4bf0775783871b6d1d910ed34676c0c24211ca66
Doğan Çeçen <sepeth@gmail.com>
2015-02-02 15:31:33 +0200

    post message endpoint'i eklendi

--- thewall.py
+++ thewall.py
@@ -1,20 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import Connection
+
+db = Connection()['thewall']
 app = Flask(__name__)
 
-db=Connection()['thewall']
-@app.route("/api/messages")
-def hello():
+
+@app.route('/api/messages')
+def list_messages():
     messages = db.messages.find()
-
     result = []
-
     for message in messages:
         result.append({
-	       "name":message.get("name"),
-	       "message":message.get("message"),
+            'name': message.get('name'),
+            'message': message.get('message'),
         })
     return jsonify(messages=result)
 
-if __name__ == "__main__":
+
+@app.route('/api/messages', methods=['POST'])
+def post_message():
+    data = {
+        'name': request.form.get('name'),
+        'message': request.form.get('message'),
+    }
+    db.messages.insert(data)
+    del data['_id']
+    return jsonify(data)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
