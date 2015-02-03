d1d24aacad27273e19746f289117ddbedfb4a5f7
Doğan Çeçen <sepeth@gmail.com>
2015-02-03 14:46:13 +0200

    accept port parameter

--- thewall.py
+++ thewall.py
@@ -41,4 +41,5 @@ def post_message():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
